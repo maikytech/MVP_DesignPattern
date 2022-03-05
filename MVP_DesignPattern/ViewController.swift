1c126972ccf6de8ab9561082ddde11aa698feed6
maikytech <maikytech1307@gmail.com>
2022-03-05 12:17:41 -0500

    Start project

---
+++ MVP_DesignPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP_DesignPattern
+//
+//  Created by Maiqui Cedeño on 5/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
