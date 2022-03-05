6274709dad02a348fe141090fe635a48460dc83a
maikytech <maikytech1307@gmail.com>
2022-03-05 13:31:00 -0500

    Add UsersViewControllers

---
+++ MVP_DesignPattern/MVP/Presenter/Presenter.swift
@@ -0,0 +1,13 @@
+//
+//  Presenter.swift
+//  MVP_DesignPattern
+//
+//  Created by Maiqui Cedeño on 5/03/22.
+//
+
+import Foundation
+
+// https://jsonplaceholder.typicode.com/users
+
+
+
--- MVP_DesignPattern/MVP/View/UsersViewController.swift
+++ MVP_DesignPattern/MVP/View/UsersViewController.swift
@@ -7,11 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class UsersViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+      
+    }
+    
+    //MARK: - Private Methods
+    private func setupView() {
+        title = "Users"
+        
     }
 
 
