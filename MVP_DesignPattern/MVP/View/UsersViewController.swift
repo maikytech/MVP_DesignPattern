487fbebc31dfaf140038b78da6e0cae7920b3ba8
maikytech <maikytech1307@gmail.com>
2022-03-05 19:25:14 -0500

    Set TableView

--- MVP_DesignPattern/MVP/View/UsersViewController.swift
+++ MVP_DesignPattern/MVP/View/UsersViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class UsersViewController: UIViewController {
+    
+    //MARK: - Variables
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +26,37 @@ class UsersViewController: UIViewController {
     //MARK: - Private Methods
     private func setupView() {
         title = "Users"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+    }
 
+}
 
+//MARK: - UITableViewDataSource
+extension UsersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
+
+//MARK: - UITableViewDelegate
+extension UsersViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Ask presenter to handle the tap.
+    }
+    
 }
 
