53820ac5618718021177198a14ddee4c13e7d9f4
maikytech <maikytech1307@gmail.com>
2022-03-05 19:06:44 -0500

    Add Presenter

---
+++ MVP_DesignPattern/MVP/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MVP_DesignPattern
+//
+//  Created by Maiqui Cedeño on 5/03/22.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let email: String
+    let userName: String
+}
--- MVP_DesignPattern/MVP/Presenter/Presenter.swift
+++ MVP_DesignPattern/MVP/Presenter/Presenter.swift
@@ -6,8 +6,45 @@
 //
 
 import Foundation
+import UIKit
 
 // https://jsonplaceholder.typicode.com/users
 
+protocol UserPresenterDelegate: AnyObject {
+    func presentUsers(users: [User])
+    
+}
+
+typealias PresenterDelegate = UserPresenterDelegate & UIViewController
+
+
+final class UserPresenter {
+    
+    private weak var delegate: PresenterDelegate?
+    
+    private func setViewDelegate(delegate: PresenterDelegate) {
+        self.delegate = delegate
+        
+    }
+    
+    private func getUsers() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let users = try JSONDecoder().decode([User].self, from: data)
+                self?.delegate?.presentUsers(users: users)
+            }catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+}
 
 
